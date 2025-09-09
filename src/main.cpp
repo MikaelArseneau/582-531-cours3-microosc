1a334c3e33969eb097e71c02b69aeffa22e16165
MikaelArseneau <mikael.arseneau@hotmail.ca>
2025-09-09 17:15:52 -0400

    exercie1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
+#include <MicroOscSlip.h>
+// Le nombre 128 entre les < > ci-dessous est le nombre maximal d'octets réservés pour les messages entrants.
+// Les messages sortants sont écrits directement sur la sortie et ne nécessitent pas de réservation d'octets supplémentaires.
+MicroOscSlip<128> monOsc(&Serial);
 
 
 #define MA_BROCHE_BOUTON 39
+#define MA_BROCHE_ANGLE 32
 
 void setup() {
   Serial.begin(115200);
@@ -12,6 +17,11 @@ void setup() {
 
 void loop() {
   int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
-  Serial.println(maLectureBouton) ;
+  int lectureAnalogique = analogRead(MA_BROCHE_ANGLE);
+  //Serial.println(maLectureBouton) ;
+  
+monOsc.sendInt( "/bouton" , maLectureBouton);
+monOsc.sendInt( "/angle" , lectureAnalogique);
+
 }
 
